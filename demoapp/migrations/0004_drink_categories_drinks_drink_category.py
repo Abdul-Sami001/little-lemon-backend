30b8fa237b1a658a5128f9d7f341ec8330b3bfa5
Abdul-Sami001 <abdulsami.kh001@gmail.com>
2023-06-18 15:12:35 +0500

    changed database from sqlite to MySql

--- demoapp/migrations/0004_drink_categories_drinks_drink_category.py
+++ demoapp/migrations/0004_drink_categories_drinks_drink_category.py
@@ -5,23 +5,34 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('demoapp', '0003_rename_name_drinks_drink_name'),
+        ("demoapp", "0003_rename_name_drinks_drink_name"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Drink_Categories',
+            name="Drink_Categories",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('drink_category', models.CharField(max_length=100)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("drink_category", models.CharField(max_length=100)),
             ],
         ),
         migrations.AddField(
-            model_name='drinks',
-            name='drink_category',
-            field=models.ForeignKey(default=0, on_delete=django.db.models.deletion.CASCADE, to='demoapp.drink_categories'),
+            model_name="drinks",
+            name="drink_category",
+            field=models.ForeignKey(
+                default=1,
+                on_delete=django.db.models.deletion.CASCADE,
+                to="demoapp.drink_categories",
+            ),
             preserve_default=False,
         ),
     ]
--- demoapp/models.py
+++ demoapp/models.py
@@ -31,5 +31,12 @@ class Booking(models.Model):
     comments = models.CharField(max_length=100)
 
 
+class Employee(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    role = models.CharField(max_length=100)
+    shift = models.IntegerField()
+
+
 def __str__(self):
     return self.name
